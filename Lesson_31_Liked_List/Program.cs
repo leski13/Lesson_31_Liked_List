c03ca8414219b8b5d5086fbb8a9790d529b27f98
leski139013 <leski139013@gmail.com>
2019-05-13 20:57:40 +0300

    Добавлена реализация двусвязного и кольцевого списков

--- Lesson_31_Liked_List/Program.cs
+++ Lesson_31_Liked_List/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lesson_31_Liked_List.Model;
+using System;
 
 namespace Lesson_31_Liked_List
 {
@@ -35,6 +36,54 @@ namespace Lesson_31_Liked_List
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            //Console.ReadLine();
+            //Двусвязный список
+            Console.WriteLine("Добавление данных в список");
+            var duplexlist = new DuplexLinkedList<int>();
+            duplexlist.Add(44);
+            duplexlist.Add(45);
+            duplexlist.Add(46);
+            duplexlist.Add(47);
+            duplexlist.Add(48);
+
+            foreach(var item in duplexlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Element deleted");
+            duplexlist.Delete(47);
+            foreach(var item in duplexlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("List reverse");
+            var reverse = duplexlist.Reverse();
+            foreach(var item in reverse)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Кольцевой список");
+            var circulatedlinkedlist = new CircularLinkedList<int>();
+            circulatedlinkedlist.Add(62);
+            circulatedlinkedlist.Add(63);
+            circulatedlinkedlist.Add(64);
+            circulatedlinkedlist.Add(65);
+            circulatedlinkedlist.Add(66);
+
+            foreach(var item in circulatedlinkedlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Element deleted");
+            circulatedlinkedlist.Delete(66);
+            circulatedlinkedlist.Delete(62);
+
+            foreach(var item in circulatedlinkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadLine();
     
         }
